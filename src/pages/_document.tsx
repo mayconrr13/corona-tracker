c73402eef89d7f4f7974b378bfd49b5cf17342bd
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-06-13 16:03:10 -0300

    add favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/svg" href="/favicon.svg" />
+          <title>Corona Tracker</title>
         </Head>
         <body>
           <Main />
